b4d2dbcba34fa211d0ff2d5a38a075415eb1a1cd
Matt Brasier <matt.brasier.external@atos.net>
2017-09-08 21:52:15 +0100

    more debug for jenkins

--- src/test/java/net/atos/ol/dummy/DummyTest.java
+++ src/test/java/net/atos/ol/dummy/DummyTest.java
@@ -43,20 +43,25 @@ public class DummyTest {
     @InSequence(10)
     public void echoTest() throws Exception {
 
-        System.out.println("URL: "+url.toExternalForm());
-        System.out.println("HOST IP: "+ip);
-        System.out.println("CUBE IP: "+ip);
-        System.out.println("EXEC HOSTNAME: "+execWithResult("hostname"));
-        System.out.println("JAVA HOSTNAME: "+InetAddress.getLocalHost().getHostName());
-        System.out.println("EXEC2 HOSTNAME: "+execWithResult("ip a | grep docker0"));
-        System.out.println("EXEC3 HOSTNAME: "+execWithResult("cat /etc/hosts | grep 172"));
-        System.out.println("EXEC4 HOSTNAME: "+execWithResult("hostname -i"));
-        System.out.println("EXEC5 HOSTNAME: "+execWithResult("ifconfig eth0 | grep \"inet addr:\" | cut -d \" \" -f 1"));
-        System.out.println("EXEC6 HOSTNAME: "+execWithResult("tail -1 /etc/hosts"));
-        System.out.println("EXEC7 HOSTNAME: "+execWithResult("/sbin/ip route | awk '/scope/ {print $9}'"));
-        System.out.println("EXEC8 HOSTNAME: "+execWithResult("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dummy-mediation"));
+        //System.out.println("URL: "+url.toExternalForm());
+        //System.out.println("HOST IP: "+ip);
+        //System.out.println("CUBE IP: "+ip);
+        //System.out.println("EXEC HOSTNAME: "+execWithResult("hostname"));
+        //System.out.println("JAVA HOSTNAME: "+InetAddress.getLocalHost().getHostName());
+        //System.out.println("EXEC2 HOSTNAME: "+execWithResult("ip a | grep docker0"));
+        //System.out.println("EXEC3 HOSTNAME: "+execWithResult("cat /etc/hosts | grep 172"));
+        //System.out.println("EXEC5 HOSTNAME: "+execWithResult("ifconfig eth0 | grep \"inet addr:\" | cut -d \" \" -f 1"));
+        //System.out.println("EXEC6 HOSTNAME: "+execWithResult("tail -1 /etc/hosts"));
+        //System.out.println("EXEC7 HOSTNAME: "+execWithResult("/sbin/ip route | awk '/scope/ {print $9}'"));
+        //System.out.println("EXEC8 HOSTNAME: "+execWithResult("docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' dummy-mediation"));
 
+        String url2 = execWithResult("hostname -i");
+        if (url2.indexOf(" ") != -1)
+        {
+            url2=url2.substring(url2.indexOf(" ")+1,url2.length());
+        }
 
+        System.out.println("EXEC4 HOSTNAME: "+url2);
 
         //System.out.println("test IP: "+InetAddress.getByName("test"));
         //System.out.println("dummy-mediator IP: "+InetAddress.getByName("dummy-mediator"));
@@ -66,11 +71,11 @@ public class DummyTest {
         given().
                 when().
                 //get(url.toExternalForm() + "dummy-mediation/echo/12345").
-                get("http://localhost:8080/" + "dummy-mediation/echo/12345").
+                get("http://"+url2+":8080/" + "dummy-mediation/echo/12345").
                 then().
                 assertThat().body(containsString("12345"));
     }
-
+/*
     @Test
     @RunAsClient
     @InSequence(20)
@@ -97,7 +102,7 @@ public class DummyTest {
                 then().
                 assertThat().body(containsString(":"));
     }
-
+*/
     private String execWithResult(String s)
     {
         String result = null;
